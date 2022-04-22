b7fbb8c232c0c3d2bba803305607e5b4a3172c2b
Daniel Mihov <daniel.mihovv@gmail.com>
2022-04-23 00:36:02 +0300

    You will receive an integer as input from the console. Print the 10 times table for this integer. See the examples below for more information.

---
+++ BasicSyntaxConditionalStatementsAndLoops/F10MultiplicationTable.java
@@ -0,0 +1,13 @@
+package BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class F10MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d X %d = %d\n", num, i, num * i);
+        }
+    }
+}
