3e08e390b8a5e5b2127a9d94de9069e64ef75b73
Daniel Mihov <daniel.mihovv@gmail.com>
2022-04-25 17:55:12 +0300

    Rewrite your program so it can receive the multiplier from the console. Print the table from the given multiplier to 10. If the given multiplier is more than 10 - print only one row with the integer, the given multiplier, and the product. See the examples below for more information.

---
+++ BasicSyntaxConditionalStatementsAndLoops/F11MultiplicationTable2_0.java
@@ -0,0 +1,18 @@
+package BasicSyntaxConditionalStatementsAndLoops;
+
+import java.util.Scanner;
+
+public class F11MultiplicationTable2_0 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+        int multiplier = Integer.parseInt(scanner.nextLine());
+        if (multiplier <= 10) {
+            for (int i = multiplier; i <= 10; i++) {
+                System.out.printf("%d X %d = %d\n", num, i, num * i);
+            }
+        } else {
+            System.out.printf("%d X %d = %d\n", num, multiplier, num * multiplier);
+        }
+    }
+}
